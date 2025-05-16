6c573537f3d02e12c4534a7ec6492da51fb59598
Terra Fantastic <ethenpickett@gmail.com>
2025-05-16 13:15:13 +1200

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ fetch("Birds.json")
   .then(response => response.json())
   .then(json => {
     const birds = json
-    console.log(birds[0].NativeBirds[0]
+    console.log(birds[0].NativeBirds[0])
   }
   );
   
