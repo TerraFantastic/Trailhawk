056a4527bc18b96f162ad7d7f980dd11b8f75dfe
Terra Fantastic <ethenpickett@gmail.com>
2025-05-16 20:39:28 +1200

    Added onClick to Buttons, Added Test Function to Check Button onClick

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ fetch("Birds.json")
   .then(json => {
 
     const birds = json
+
+    function logclick(selected) {
+      console.log("Button Pushed")
+      console.log(selected.Name)
+    }
     
     function createbirdbuttons(buttons) {
       birds.NativeBirds.forEach(element => {
@@ -10,8 +15,9 @@ fetch("Birds.json")
       newbutton.innerHTML = element.Name
       newbutton.style.width = "50px"
       newbutton.style.height = "50px"
+      newbutton.onclick = logclick(element)
       document.querySelector('#infowindow').append(newbutton)
      })
-    }    
+    }
+
     createbirdbuttons(birds);
-  });
