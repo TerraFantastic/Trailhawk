feacc7744c00c7553513ea3fc01f762547ecd422
Terra Fantastic <ethenpickett@gmail.com>
2025-05-16 18:29:24 +1200

    create functions for generating bird buttons

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 fetch("Birds.json")
   .then(response => response.json())
   .then(json => {
+
     const birds = json
-    birds.NativeBirds.forEach(element => {
+    
+    function createbirdbuttons(buttons) {
+      birds.NativeBirds.forEach(element => {
       let newbutton = document.createElement('button')
       newbutton.innerHTML = element.Name
       newbutton.style.width = "50px"
       newbutton.style.height = "50px"
       document.querySelector('#infowindow').append(newbutton)
-    });
-  }
-  );
-  
+     }
+    }
+    createbirdbuttons(birds)
+  }  
+);
   
