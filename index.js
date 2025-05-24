9855c5a1f6c4d80d6138b6b65c0c579c7d6b5e4f
Terra Fantastic <ethenpickett@gmail.com>
2025-05-24 15:01:48 +1200

    Added Comments for showbirdinfo function, added creation of heading element with bird name append to infowindow div, moved change species selection button to under species image in infowindow

--- index.js
+++ index.js
@@ -6,25 +6,35 @@ fetch("Birds.json")
 
     function showbirdinfo(selected) {
 
+      // Clear all elements from the infowindow div
       document.querySelector('#infowindow').innerHTML = ""
 
-      let backbutton = document.createElement('button')
-      backbutton.innerHTML = "Change Selected Species"
-      backbutton.id = "backbutton"
-      document.querySelector('#infowindow').append(backbutton)
-      document.querySelector('#infowindow').append(document.createElement('br'))
-      document.getElementById("backbutton").addEventListener('click', () => {
-        createbirdbuttons(birds) })
-
+      // Find data for selected bird species
       birds.NativeBirds.forEach(element => {
         if (element.Name == selected) {
+          
+          // Create heading with the bird name
+          let birdname = document.createElement('h3')
+          birdname.innerHTML = element.Name
+          document.querySelector('#infowindow').append(birdname)
 
+          // Display bird image
           let birdimage = document.createElement('img')
           birdimage.src = `pictures/${element.Name}.jpg`
           birdimage.style.width = "20vw"
           birdimage.style.height = "20vw"
           document.querySelector('#infowindow').append(birdimage)
 
+          // Create a back button
+          let backbutton = document.createElement('button')
+          backbutton.innerHTML = "Change Selected Species"
+          backbutton.id = "backbutton"
+          document.querySelector('#infowindow').append(backbutton)
+          document.querySelector('#infowindow').append(document.createElement('br'))
+          document.getElementById("backbutton").addEventListener('click', () => {
+          createbirdbuttons(birds) })
+          
+          // Display species information
           let newinfowindow = document.createElement('p')
           newinfowindow.innerHTML = element.Desc
           document.querySelector('#infowindow').append(newinfowindow)
